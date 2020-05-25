1d20418cdfd05857049884d425664d64bf6d3ae5
Javier Graña <javiergrana@hotmail.com>
2020-05-25 17:26:22 -0300

    Agregando ejemplo de arreglos al repositorio

---
+++ Clase7/ManejadorDeTareas/ManejadorDeTareas/ClasesParaTarea/Tarea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClasesParaTarea
+{
+    public class Tarea
+    {
+        private int importancia; 
+        private string nombreDeTarea;
+        private DateTime fecha;
+
+        public int Importancia { get => importancia; set => importancia = value; }
+        public string NombreDeTarea { get => nombreDeTarea; set => nombreDeTarea = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+    }
+}
---
+++ Clase7/ManejadorDeTareas/ManejadorDeTareas/TareaConsola/Program.cs
@@ -0,0 +1,25 @@
+﻿using ClasesParaTarea;
+using System;
+
+namespace TareaConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tarea[] MisTareas = new Tarea[10];
+
+            for (int i = 0; i < MisTareas.Length; i++)
+            {
+                Tarea Mitarea = new Tarea();
+                Mitarea.Importancia = i;
+                Mitarea.NombreDeTarea = "Tarea Nro " + i;
+                Mitarea.Fecha = DateTime.Now.AddDays(i);
+                MisTareas[i] = Mitarea;
+
+                Console.WriteLine(MisTareas[i].NombreDeTarea);
+
+            }
+        }
+    }
+}
