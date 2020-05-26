73b026cd0ba6c08af56c14b12ea3fe5b36c90d96
Javier Graña <javiergrana@hotmail.com>
2020-05-25 22:18:57 -0300

    Manejador de tareas con Listas

--- Clase7/ManejadorDeTareas/ManejadorDeTareas/ClasesParaTarea/Tarea.cs
+++ Clase7/ManejadorDeTareas/ManejadorDeTareas/ClasesParaTarea/Tarea.cs
@@ -4,14 +4,23 @@ using System.Text;
 
 namespace ClasesParaTarea
 {
+    public enum EstadoTarea
+    { 
+        Pendiente = 0,
+        Haciendo = 2,
+        Concluida = 3
+    }
+
     public class Tarea
     {
         private int importancia; 
         private string nombreDeTarea;
         private DateTime fecha;
+        EstadoTarea estado;
 
         public int Importancia { get => importancia; set => importancia = value; }
         public string NombreDeTarea { get => nombreDeTarea; set => nombreDeTarea = value; }
         public DateTime Fecha { get => fecha; set => fecha = value; }
+        public EstadoTarea Estado { get => estado; set => estado = value; }
     }
 }
---
+++ Clase7/ManejadorDeTareas/ManejadorDeTareas/TareaConsolaConListas/Program.cs
@@ -0,0 +1,42 @@
+﻿using ClasesParaTarea;
+using System;
+using System.Collections.Generic;
+
+namespace TareaConsolaConListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tarea> ListaDeDatos = new List<Tarea>();
+
+            string salida = "";
+            int num = 0;
+            do
+            {
+                Console.WriteLine("Ingrese la descripción de la tarea:");                
+                string DescTarea = Console.ReadLine();
+
+                Tarea Ntarea = new Tarea();
+                Ntarea.NombreDeTarea = DescTarea;
+                Ntarea.Importancia = num++;
+                Ntarea.Fecha = System.DateTime.Now.AddDays(num);
+                Ntarea.Estado = EstadoTarea.Pendiente;
+
+                ListaDeDatos.Add(Ntarea);
+
+                Console.WriteLine("¿Desea ingrear una nueva tarea (s/n)?");
+                salida = Console.ReadLine();
+            } while (salida != "n");
+
+            ListaDeDatos[0].Estado = EstadoTarea.Concluida;
+
+            foreach (Tarea TareaX in ListaDeDatos)
+            {
+                Console.WriteLine(TareaX.Importancia + " - " + TareaX.NombreDeTarea +" - " + TareaX.Estado);
+            }
+
+
+        }
+    }
+}
